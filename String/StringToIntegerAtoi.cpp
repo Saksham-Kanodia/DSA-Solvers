381bc69f1125a189edeb3f7346b59bab3327a7d1
Saksham Kanodia <sakshamkanodia2500@gmail.com>
2026-03-21 18:27:19 +0530

    String to Integer (atoi) Problem completed.

--- String/StringToIntegerAtoi.cpp
+++ String/StringToIntegerAtoi.cpp
@@ -22,10 +22,10 @@ int stringToInt(string s){
 
     while(s[idx]>='0' && s[idx]<='9'){
 
-        if(res>INT_MAX/10||res==(INT_MAX/10) && s[idx]-'0'>7)
+        if(res>INT_MAX/10||(res==(INT_MAX/10) && s[idx]-'0'>7))
         return sign == 1 ? INT_MAX:INT_MIN;
 
-        res = 10*res + (s[idx]-'0');
+        res = 10*res + (s[idx++]-'0');
 
     }
 
