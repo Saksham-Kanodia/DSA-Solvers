fb311a2e375d3621bf7f5b871ad88b1000a6b568
Saksham Kanodia <sakshamkanodia2500@gmail.com>
2026-04-02 19:39:16 +0530

    Traversal of Linked List completed.

---
+++ Linked List/TraversalInLL.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    public:
+    Node(int data1, Node* next1){
+        data = data1;
+        next = next1;
+    }
+
+    public:
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+
+Node* convertArr2LL(vector <int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+
+    for(int i=1; i<arr.size(); i++){
+        Node* temp = new Node(arr[i]);
+        mover->next=temp;
+        mover = temp;
+    }
+
+    return head;
+}
+
+void traversalOfLL(Node* head){
+    Node* temp = head;
+
+    while(temp!=nullptr){
+       cout << temp->data << " ";
+       temp = temp-> next;
+    }
+}
+
+int main(){
+
+    vector <int> arr = {2,1,3,5,8};
+    traversalOfLL(convertArr2LL(arr));
+
+    return 0;
+}
