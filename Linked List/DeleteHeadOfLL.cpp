321fb957ad76aaab1e99c24c47afc853d80ee738
Saksham Kanodia <sakshamkanodia2500@gmail.com>
2026-04-03 11:22:43 +0530

    Deleted the Head Of Linked List.

---
+++ Linked List/DeleteHeadOfLL.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node{
+    public: 
+    int data;
+    Node* next;
+
+    public:
+    Node(int data1, Node* next1){
+        data=data1;
+        next=next1;
+    }
+
+    public:
+    Node(int data1){
+        data=data1;
+        next=nullptr;
+    }
+};
+
+Node* convertArr2LL(vector <int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+
+    for(int i=1; i<arr.size(); i++){
+        Node* temp = new Node(arr[i]);
+        mover->next= temp;
+        mover = temp;
+    }
+
+    return head;
+}
+
+Node* deleteHeadOfLL(Node* head){
+    Node* temp = head;
+    head= head-> next;
+    delete temp;
+    return head;
+}
+
+int main(){
+    vector <int> arr = {3,1,5,6,7};
+    cout << deleteHeadOfLL(convertArr2LL(arr))-> data;
+    
+    return 0;
+}
